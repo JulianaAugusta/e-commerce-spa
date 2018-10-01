41a5fbe681505a7af00df16f9442379986b1e83e
Juliana Augusta <juli_aug@hotmail.com>
2018-09-30 23:54:07 -0300

    deploy no firebase após atualizações

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,48 +1,114 @@
-
-
-
 function renderIndex() {
-return `
-<div id="carouselExampleSlidesOnly" class="carousel slide container-fluid" data-ride="carousel">
-<div class="carousel-inner">
-  <div class="carousel-item active text-center">
-    <img class="d-block img-thumbnail img-fluid" src="/dist/images/1eaa98bfe7d08d914944143cb797ba4d.jpg" alt="First slide">
+  return `
+  <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="../dist/images/regular_paraiso-moda-bebe-foto-catalogo-100.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../dist/images/regular_paraiso-moda-bebe-foto-catalogo-54.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../dist/images/regular_paraiso-moda-bebe-foto-catalogo-23.jpg" alt="Third slide">
+    </div>
   </div>
-  <div class="carousel-item text-center">
-    <img class="d-block img-thumbnail img-fluid" src="/dist/images/2018-new-fashion-baby-boy-and-girl-clothes-long-sleeve-cartoon-pattern-one-piece-baby-rompers.jpg" alt="Second slide">
-  </div>
-  <div class="carousel-item text-center">
-    <img class="d-block img-thumbnail img-fluid" src="/dist/images/New-Arriving-fashion-baby-Girl-suit-sets-vest-dress-2pcs-set-baby-suit-Baby-Clothes-Set.jpg_640x640.jpg" alt="Third slide">
-  </div>
-  <div class="carousel-item text-center">
-    <img class="d-block img-thumbnail img-fluid" src="/dist/images/24b331ac1aa0f098f7dbd56148b32423.jpg" alt="Second slide">
-  </div>
-</div>
 </div>`
 }
 
-
-function renderProductsList() {
-  return `<div class="container"><div class="row">${produtos.map(product => `
-  <div class="col-lg-2 produto" data-id="${product.id}">
-    <div class="single-product-wrapper">
-      <div class="product-img">
-        <img class="imagens" src="${product.thumbnail}" alt="">
+function renderProductsList(products) {
+  return `
+  <div class="container"><div class="row">
+  ${products.map((product =>`<div class="col-lg-2 produto" data-id="${product.id}">
+      <div class = "single-product-wrapper" >
+        <div class="product-img">
+          <img class="imagens"src="${product.thumbnail}" alt="">
+        </div>
+        <div class="product-description"><h6>${product.title}</h6>
+          <p class="product-price"> R$ ${product.price}</p>
+          <div class="hover-content">
+            <div class="add-to-cart-btn">
+              <a href="product/${product.id}" class="btn essence-btn">Ver produto</a>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="product-description">
-        <h6>${product.title}</h6>
-        <p class="product-price">R$${product.price}</p>
-        <div class="hover-content">
-          <div class="add-to-cart-btn">
-            <a href="product/${product.id}" class="btn essence-btn">Ver produto</a>
+    </div>`)).join("")}</div></div>
+  `;
+}
+
+function renderProduct(product) {
+  return `
+  <div class="container"><div class="row area-produto">
+    <div class="col-lg-10 produto produtoItem" data-id="${product.id}">
+      <div class = "single-product-wrapper" >
+      <h6>${product.title}</h6>
+        <div class="product-img">
+          <img class="imagens"src="${product.thumbnail}" alt="">
+        </div>
+        <div class="product-description">
+          <p class="product-price"> R$ ${product.price}</p>
+          <div class="hover-content">
+            <div class="add-to-cart-btn">
+             <button onclick="addCarrinho()" type="button">Adicionar no carrinho</button>
+            </div>
+          </div>
+          </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-  `).join("")}</div></div>`
+    `;
 }
+
+function renderCarrinho () {
+
+  return `
+  <h2 class="mt-0 title-card mt-5 ml-5"> Carrinho de compras</h2>
+  <h2 class="mt-0 title-card ml-5">produtos adicionados </h2>
+  <div class=" item-card media">
+  <img id="img" src="productImg" "width="90" height="90">
+  <div class="media-body">
+
+    <p id="produto" > </p>
+    <p id="price" class="mb-0 font-weight-bold">   </p>
+    <div class ="d-flex justify-content-end">
+    <button class= " btn btn-color btn-sm"onClick="removeItem()" class ="demo" id'remove'> Remover Item </button>
+    </div >
+    <p class="mb-0"> </p>
+  </div>
+</div>
+  `
+}
+
+function addCarrinho(produto) {
+
+  productInf = produto;
+  console.log(productInf)
+
+  productTitle = productInf[0]
+  console.log(productTitle)
+  productImg = productInf[1]
+  console.log(productImg)
+  productPrice = productInf[2]
+  console.log(productPrice)
+
+  localStorage.setItem('title', productTitle);
+  localStorage.setItem('img', productImg);
+  localStorage.setItem('price', productPrice);
+
+
+}
+
+function removeItem (){
+alert ('item removido, atualize a página!');
+console.log('remove')
+
+localStorage.removeItem('title');
+localStorage.removeItem('price');
+localStorage.removeItem('img');
+}
+
 
 $('.carousel').carousel({
   interval: 2000
-});
+})
--- public/dist/routes.js
+++ public/dist/routes.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
   page('/', index);
   page('/products', products);
-  // page('/product/:productId', product);
-  page()
+  page('/product/:productId', product);
+  page ('/carrinho', carrinho );
+  page();
 });
 
 function index() {
@@ -10,28 +11,36 @@ function index() {
   $("#btnProducts").html("<a href='/products'>Ver produtos</a>");
 }
 
-function products() {
-  getProducts();
-  $("#btnProducts").html("<a href='/'> Home </a>");
+async function products() {
+  const mercadoLivreProducts = await getProducts();
+  $("main").html(renderProductsList(mercadoLivreProducts));
+  $("#btnProducts").html("<a href='/'>Voltar ao início</a>");
 }
 
-let produtos = [];
-
-function erro() {
-  console.log("erro");
-}
-
-function carregaPosts(data) {
-  produtos = data.results;
-  $("main").html(renderProductsList(produtos));
+async function product(context) {
+  const mercadoLivreProduct = await getProduct(context.params.productId);
+  $("main").html(renderProduct(mercadoLivreProduct));
+  $("#btnProducts").html("<a href='/'>Voltar ao início</a>");
 }
 
 function getProducts() {
-  const url = `https://api.mercadolibre.com/sites/MLB/search?category=MLB1384`
-  $.ajax({
-    type: "GET",
-    url: url,
-    success: carregaPosts,
-    error: erro
-  });
+  return fetch("https://api.mercadolibre.com/sites/MLB/search?category=MLB1384")
+    .then((response) => response.json())
+    .then((json) => json.results);
+}
+
+function getProduct(id) {
+  return fetch(`https://api.mercadolibre.com/items/${id}?
+    access_token=APP_USR-2203506221789612-092014-4da1823b26643e8947e11e50342db4aa-315570596`)
+    .then((response) => response.json())
+    .then((json) => json);
+}
+
+function carrinho(){
+
+    $("main").html(renderCarrinho());
+    document.getElementById("produto").innerHTML = localStorage.getItem("title");
+    document.getElementById("img").src = localStorage.getItem("img");
+    document.getElementById("price").innerHTML = 'Valor R$ '+ localStorage.getItem("price");
+
 }
